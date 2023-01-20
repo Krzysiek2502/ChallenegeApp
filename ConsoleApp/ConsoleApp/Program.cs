130f7a333a7d6403372b982a1b6fbcd9dca46977
Krzysztof Kanior <kkanior1990@wp.pl>
2023-01-20 18:23:51 +0100

    Zadanie dzień 4

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "kobieta";
+int age = 27;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name=="Ewa"&&age<30)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if (sex == "mężczyzna")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
